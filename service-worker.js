5ae6ba1b47f9bd225699d4a2f8ee4182b31724ef
olsenjorn <olsen.jorn@gmail.com>
2026-06-13 16:28:22 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,15 @@
 
+const CACHE_NAME = 'alfa-kompass-v1';
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  // Foreløpig gjør vi ingenting risikabelt.
+  // Dette er bare grunnmuren for PWA.
+});
